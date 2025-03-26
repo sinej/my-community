dc363f3d2dfcbb29063d2ebf82331a69482ad93e
sinej <sinej1020@gmail.com>
2025-03-26 21:59:40 +0900

    feat: text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,41 @@
-import { StyleSheet, View} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+} from 'react-native';
+import {useState} from "react";
 
 export default function HomeScreen() {
+
+  const [text, setText] = useState('');
   return (
-    <View></View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.text}>텍스트</Text>
+      </View>
+      <TextInput placeholder={'이름을 적어주세요'}
+                 value={text}
+                 onChangeText={(value) => setText(value)}
+                 style={styles.input}
+      />
+      <Button title="확인" onPress={() => console.log("Hello")} />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'yellow',
+  },
+  text: {
+    color: '#000',
+    fontSize: 20,
+  },
+  input: {
+    fontSize: 20,
+    fontWeight: 500,
+  }
+});
