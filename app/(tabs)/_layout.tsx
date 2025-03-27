b655e90bec899e0ba87500e99d342c6e2f5ff5e4
sinej <sinej1020@gmail.com>
2025-03-27 23:22:29 +0900

    feat: tabs & button 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
 
@@ -14,12 +15,27 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "home-sharp" : 'home-outline'} size={24} color="black" />
+          )
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="my"
         options={{
-          title: 'Explore',
+          title: 'My',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "person-circle-sharp" : "person-circle-outline"} size={24} color="black" />
+          )
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: 'Setting',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? "settings-sharp" : "settings-outline"} size={24} color="black" />
+          )
         }}
       />
     </Tabs>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,67 +1,26 @@
+import { router } from 'expo-router';
 import {
   SafeAreaView,
   View,
+  Pressable,
+  Text,
   StyleSheet,
 } from 'react-native';
+import CustomButton from "@/components/customButton";
 
 export default function HomeScreen() {
 
   return (
-    <SafeAreaView style={styles.parent}>
-      <View style={styles.container1}>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </View>
-      <View style={styles.container2}>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </View>
-      <View style={styles.container3}>
-        <Box></Box>
-        <Box></Box>
-        <Box></Box>
-      </View>
+    <SafeAreaView>
+      <Pressable>
+        <Text>홈스크린</Text>
+        <CustomButton label='버튼' onPress={() => {}} />
+      </Pressable>
     </SafeAreaView>
   );
 }
 
 
-function Box() {
-  return <View style={styles.box} />;
-}
-
 
 const styles = StyleSheet.create({
-  parent: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#fff',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: "space-between"
-  },
-  container1: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    gap: 2,
-  },
-  container2: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-  },
-  container3: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    gap: 2,
-  },
-  box: {
-    width: 50,
-    height: 50,
-    backgroundColor: 'red',
-  },
 });
