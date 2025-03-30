97dc7b8952d607d232a4dc013a6f0ee0dcffb950
sinej <sinej1020@gmail.com>
2025-03-30 23:40:14 +0900

    feat: logo 추가

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack, Link } from "expo-router";
 import Foundation from "@expo/vector-icons/Foundation"
+import {colors} from "@/constants";
 interface AuthLayoutType {
 
 };
@@ -7,7 +8,12 @@ interface AuthLayoutType {
 const AuthLayout = (props: AuthLayoutType) => {
   const {} = props;
   return (
-    <Stack>
+    <Stack screenOptions={{
+      headerTintColor: colors.BLACK,
+      contentStyle: {
+        backgroundColor: colors.WHITE
+      }
+    }}>
       <Stack.Screen name="index"
                     options={{
                       title: '로그인',
--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, Text } from "react-native";
+import {SafeAreaView, StyleSheet, View, Image} from "react-native";
 
 interface AuthScreenType {
 
@@ -7,10 +7,26 @@ interface AuthScreenType {
 const AuthScreen = (props: AuthScreenType) => {
     const {} = props;
     return (
-        <SafeAreaView>
-          <Text></Text>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.logoContainer}>
+            <Image source={require('@/assets/images/logo.png')} style={styles.logo} />
+          </View>
         </SafeAreaView>
     );
 }
 
 export default AuthScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    width: 112,
+    height: 112,
+  }
+})
